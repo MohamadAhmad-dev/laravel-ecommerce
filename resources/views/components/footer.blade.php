88a876a8eef2ebe996921c074ce1036310cb9746
Mohamad Ahmad <mohammad.ahmad.dev@gmail.com>
2026-09-09 22:00:57 +0300

    Create layout navbar and footer components

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,20 +1,13 @@
-<x-Layout>
-
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('assets/styles/style.css')}}">
-    <title>Welcome Page</title>
+    <title>Footer</title>
 </head>
 <body>
-
-    <main>
-            <h1>My Shop</h1>
-    </main>
-        
+    <footer>
+        Mohamad Hussein Ahmad | 2026
+    </footer>
 </body>
 </html>
-
-</x-Layout>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,20 +1,15 @@
-<x-Layout>
-
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('assets/styles/style.css')}}">
-    <title>Welcome Page</title>
+    <title>Navbar</title>
 </head>
 <body>
-
-    <main>
-            <h1>My Shop</h1>
-    </main>
-        
+    <nav>
+        <a href="">Home</a>
+        <a href="">Login</a>
+        <a href="">Signup</a>
+    </nav>
 </body>
 </html>
-
-</x-Layout>
