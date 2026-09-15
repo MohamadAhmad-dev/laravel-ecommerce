a44942ecfed4c9d82cf0859fb9f14860bff6eadb
Mohamad Ahmad <mohammad.ahmad.dev@gmail.com>
2026-09-15 22:49:23 +0300

    Build Home Page with CSS styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,138 @@
 <x-Layout title="Home">
 
-<h1>Home Page</h1>
+<h1 id="homeHeader">Featured Products</h1>
 
+<p>Handpicked recommendations for you</p>
+
+<main id="productsMain">
+
+<div class="productDiv">
+
+    <div class="productImageDiv">
+        <img src="{{asset('assets/images/A16.jpg')}}"/>
+    </div>
+    
+    <h2>Samsung A16</h2>
+    <p>a budget-friendly smartphone known for offering essential everyday performance, a vibrant display, and unusually long-term software support for its price point.</p>
+    <p>$180</p>
+
+    <div class="productButtons">
+        <button>View</button>
+        <button>Add to Cart</button>
+    </div>
+</div>
+
+<div class="productDiv">
+
+    <div class="productImageDiv">
+        <img src="{{asset('assets/images/A16.jpg')}}"/>
+    </div>
+    
+    <h2>Samsung A16</h2>
+    <p>a budget-friendly smartphone known for offering essential everyday performance, a vibrant display, and unusually long-term software support for its price point.</p>
+    <p>$180</p>
+
+    <div class="productButtons">
+        <button>View</button>
+        <button>Add to Cart</button>
+    </div>
+</div>
+
+<div class="productDiv">
+
+    <div class="productImageDiv">
+        <img src="{{asset('assets/images/A16.jpg')}}"/>
+    </div>
+    
+    <h2>Samsung A16</h2>
+    <p>a budget-friendly smartphone known for offering essential everyday performance, a vibrant display, and unusually long-term software support for its price point.</p>
+    <p>$180</p>
+
+    <div class="productButtons">
+        <button>View</button>
+        <button>Add to Cart</button>
+    </div>
+</div>
+
+<div class="productDiv">
+
+    <div class="productImageDiv">
+        <img src="{{asset('assets/images/A16.jpg')}}"/>
+    </div>
+    
+    <h2>Samsung A16</h2>
+    <p>a budget-friendly smartphone known for offering essential everyday performance, a vibrant display, and unusually long-term software support for its price point.</p>
+    <p>$180</p>
+
+    <div class="productButtons">
+        <button>View</button>
+        <button>Add to Cart</button>
+    </div>
+</div>
+
+<div class="productDiv">
+
+    <div class="productImageDiv">
+        <img src="{{asset('assets/images/A16.jpg')}}"/>
+    </div>
+    
+    <h2>Samsung A16</h2>
+    <p>a budget-friendly smartphone known for offering essential everyday performance, a vibrant display, and unusually long-term software support for its price point.</p>
+    <p>$180</p>
+
+    <div class="productButtons">
+        <button>View</button>
+        <button>Add to Cart</button>
+    </div>
+</div>
+
+<div class="productDiv">
+
+    <div class="productImageDiv">
+        <img src="{{asset('assets/images/A16.jpg')}}"/>
+    </div>
+    
+    <h2>Samsung A16</h2>
+    <p>a budget-friendly smartphone known for offering essential everyday performance, a vibrant display, and unusually long-term software support for its price point.</p>
+    <p>$180</p>
+
+    <div class="productButtons">
+        <button>View</button>
+        <button>Add to Cart</button>
+    </div>
+</div>
+
+<div class="productDiv">
+
+    <div class="productImageDiv">
+        <img src="{{asset('assets/images/A16.jpg')}}"/>
+    </div>
+    
+    <h2>Samsung A16</h2>
+    <p>a budget-friendly smartphone known for offering essential everyday performance, a vibrant display, and unusually long-term software support for its price point.</p>
+    <p>$180</p>
+
+    <div class="productButtons">
+        <button>View</button>
+        <button>Add to Cart</button>
+    </div>
+</div>
+
+<div class="productDiv">
+
+    <div class="productImageDiv">
+        <img src="{{asset('assets/images/A16.jpg')}}"/>
+    </div>
+    
+    <h2>Samsung A16</h2>
+    <p>a budget-friendly smartphone known for offering essential everyday performance, a vibrant display, and unusually long-term software support for its price point.</p>
+    <p>$180</p>
+
+    <div class="productButtons">
+        <button>View</button>
+        <button>Add to Cart</button>
+    </div>
+</div>
+
+</main>
 </x-Layout>
